e54dfa3f0e9e0082f6d0a3c0ef4c07157aee8ad5
varunk01@dcs.bbk.ac.uk <varunk01@dcs.bbk.ac.uk>
2018-05-28 18:49:33 +0100

    made changes

--- bash/q.py
+++ bash/q.py
@@ -4,13 +4,11 @@
 * Program name : qnnn.py
 * Author : varunk01
 * Purpose  : Attempts to solve the question nnn from the exam paper
-* Date created : 28/05/2018
+* Date created : 29/05/2018
 *
 * Date           Author           Ver     Comment
-* 28/05/2018     varunk01         0.1     Initial Version
+* 29/05/2018     varunk01         0.1     Initial Version
 **********************************************************************
 """
-
-
 
 #if __name__ == '__main__':
--- bash/setup.sh
+++ bash/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 c=6
 
-pip install -U pytest
+#pip install -U pytest
 
 while [ $c -ge 1 ];  do
-	cp q.py q$c.py
-	sed -i -e 's/nnn/'"$c"'/g' q$c.py
+	mkdir q$c
+	cp q.py ./q$c/q$c.py
+	sed -i -e 's/nnn/'"$c"'/g' ./q$c/q$c.py
 	((c--))
 done
-rm q.py
 
 read -p "Press enter to exit"
